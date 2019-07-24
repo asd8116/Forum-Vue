f93f6871c43420040a32cf7915082e29a217b3b4
wanaka <asd8116@gmail.com>
2019-07-25 03:18:20 +0800

    Install bootstrap and init files

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="restaurant-forum">
     <router-view />
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export default {
+  name: 'App'
 }
+</script>
+
+<style>
 </style>
