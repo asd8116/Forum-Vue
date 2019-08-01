a74cc5843b0b2ed8e60b344199a90cb8121d0b53
wanaka <asd8116@gmail.com>
2019-08-01 09:17:58 +0800

    Add SignIn page & SignUp page

--- src/router.js
+++ src/router.js
@@ -6,6 +6,16 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/signin',
+      name: 'sign-in',
+      component: () => import('./views/SignIn.vue')
+    },
+    {
+      path: '/signup',
+      name: 'sign-up',
+      component: () => import('./views/SignUp.vue')
+    },
+    {
       path: '*',
       name: 'not-found',
       component: () => import('./views/NotFound.vue')
