f9ba6ca3fb6003f34bb523b59918dc685dd73200
wanaka <asd8116@gmail.com>
2019-08-01 10:11:30 +0800

    Refactor NavTabs with v-for

--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -1,16 +1,40 @@
 <template>
   <ul class="nav nav-tabs mb-4">
-    <li class="nav-item">
-      <router-link to="/restaurants" class="nav-link">首頁</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/restaurants/feeds" class="nav-link">最新動態</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/restaurants/top" class="nav-link">TOP 10 人氣餐廳</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/users/top" class="nav-link">美食達人</router-link>
+    <li v-for="tab in tabs" :key="tab.id" class="nav-item">
+      <router-link :to="tab.path" class="nav-link">{{ tab.title }}</router-link>
     </li>
   </ul>
 </template>
+
+<script>
+import uuid from 'uuid/v4'
+
+export default {
+  data() {
+    return {
+      tabs: [
+        {
+          id: uuid(),
+          title: '首頁',
+          path: '/restaurants'
+        },
+        {
+          id: uuid(),
+          title: '最新動態',
+          path: '/restaurants/feeds'
+        },
+        {
+          id: uuid(),
+          title: 'TOP 10 人氣餐廳',
+          path: '/restaurants/top'
+        },
+        {
+          id: uuid(),
+          title: '美食達人',
+          path: '/users/top'
+        }
+      ]
+    }
+  }
+}
+</script>
--- src/views/RestaurantsFeeds.vue
+++ src/views/RestaurantsFeeds.vue
@@ -2,6 +2,17 @@
   <div class="container py-5">
     <NavTabs />
     <h1 class="mt-5">最新動態</h1>
+    <hr />
+    <div class="row">
+      <div class="col-md-6">
+        <h3>最新餐廳</h3>
+        <!-- 最新餐廳 NewestRestaurants -->
+      </div>
+      <div class="col-md-6">
+        <!-- 最新評論 NewestComments-->
+        <h3>最新評論</h3>
+      </div>
+    </div>
   </div>
 </template>
 
