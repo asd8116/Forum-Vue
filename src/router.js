90f39b5d336c2da1a0a90a7086f4a48a13d4d73a
wanaka <asd8116@gmail.com>
2019-08-01 09:46:47 +0800

    Add router and empty pages

--- src/router.js
+++ src/router.js
@@ -1,19 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import SignIn from './views/SignIn.vue'
+import Restaurants from './views/Restaurants.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/',
+      name: 'root',
+      redirect: '/restaurants'
+    },
+    {
       path: '/signin',
       name: 'sign-in',
-      component: () => import('./views/SignIn.vue')
+      component: SignIn
     },
     {
       path: '/signup',
       name: 'sign-up',
       component: () => import('./views/SignUp.vue')
+    },
+    {
+      path: '/restaurants',
+      name: 'restaurants',
+      component: Restaurants
+    },
+    {
+      path: '/restaurants/feeds',
+      name: 'restaurants-feeds',
+      component: () => import('./views/RestaurantsFeeds.vue')
+    },
+    {
+      path: '/restaurants/top',
+      name: 'restaurants-tops',
+      component: () => import('./views/RestaurantsTop.vue')
+    },
+    {
+      path: '/users/top',
+      name: 'users-top',
+      component: () => import('./views/UsersTop.vue')
     },
     {
       path: '*',
