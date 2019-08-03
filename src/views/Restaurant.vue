6bde4ea5fd795bd1abc58be9deaea1c18836d5d6
wanaka <asd8116@gmail.com>
2019-08-03 11:54:29 +0800

    Add RestaurantDetail component

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="container py-5">
     <h1>餐廳描述頁</h1>
     <!-- 餐廳資訊頁 RestaurantDetail -->
+    <RestaurantDetail :initial-restaurant="restaurant" />
     <hr />
     <!-- 餐廳評論 RestaurantComments -->
     <!-- 新增評論 CreateComment -->
@@ -10,6 +11,8 @@
 </template>
 
 <script>
+import RestaurantDetail from '@/components/RestaurantDetail'
+
 const dummyData = {
   restaurant: {
     id: 1,
@@ -95,6 +98,9 @@ export default {
       }
       this.restaurantComments = dummyData.restaurant.Comments
     }
+  },
+  components: {
+    RestaurantDetail
   }
 }
 </script>
