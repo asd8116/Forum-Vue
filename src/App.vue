becf53167254e05af938275c0af9fceb15e7e511
wanaka <asd8116@gmail.com>
2019-08-07 15:45:46 +0800

    Add stylesheets for the restaurant forum

--- src/App.vue
+++ src/App.vue
@@ -20,5 +20,8 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+main {
+  margin-top: 65px !important;
+}
 </style>
--- src/components/CreateComment.vue
+++ src/components/CreateComment.vue
@@ -67,3 +67,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.form-group {
+  margin: 21px 0 8px;
+}
+</style>
